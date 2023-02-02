e4a7fd392ccf131bcd900f451a213955dcaa09e0
faganjj <jfagan@MacBook-Air.local>
2023-02-01 20:12:01 -0500

    Added ability to view all topics, delete entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -12,9 +12,16 @@ def index(request):
 	return render(request, 'learning_logs/index.html')
 
 @login_required
-def topics(request):
+def topics(request, scope):
 	""" Show all topics """
-	topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+	if scope == 'my':
+		topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+	else:
+		topics = Topic.objects.all().order_by('date_added')
+		for topic in topics:
+			username = topic.owner.username
+			if topic.owner != request.user:
+				topic.text += "  (" + topic.owner.username + ")"
 	context = {'topics': topics}
 	return render(request, 'learning_logs/topics.html', context)
 
@@ -23,11 +30,11 @@ def topic(request, topic_id):
 	""" Show a single topic nd all it's entries """
 	topic = Topic.objects.get(id=topic_id)
 	# Make sure the topic belongs to the current user
-	if topic.owner != request.user:
-		raise Http404
+	#if topic.owner != request.user:
+	#	raise Http404
 
 	entries = topic.entry_set.order_by('-date_added')
-	context = {'topic': topic, 'entries': entries}
+	context = {'topic': topic, 'entries': entries, 'user': request.user}
 	return render(request, 'learning_logs/topic.html', context)	
 
 @login_required
@@ -43,7 +50,7 @@ def new_topic(request):
 			new_topic = form.save(commit=False)
 			new_topic.owner = request.user
 			new_topic.save()
-			return redirect('learning_logs:topics')
+			return redirect('learning_logs:topics', 'my')
 
 	# Display a blank or invalid form
 	context = {'form': form}
@@ -90,3 +97,13 @@ def edit_entry(request, entry_id):
 
 	context = {'entry': entry, 'topic': topic, 'form': form}
 	return render(request, 'learning_logs/edit_entry.html', context)
+
+@login_required
+def delete_entry(request, entry_id):
+	""" Delete an existing entry """
+	entry = Entry.objects.get(id=entry_id)
+	topic = entry.topic
+	if topic.owner != request.user:
+		raise Http404
+	entry.delete()
+	return redirect('learning_logs:topic', topic_id=topic.id)
