65898329bea7f46f9abefb9ca98bcdbbd8df09f6
faganjj <jfagan@MacBook-Air.local>
2023-04-07 22:41:07 -0400

    attempt #4 to fix sys.argv issue

--- faganweb/settings.py
+++ faganweb/settings.py
@@ -151,14 +151,12 @@ if DEVELOPMENT_MODE is True:
         }
     }
 
-# elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
-elif len(sys.argv) > 0:
-    if sys.argv[1] != 'collectstatic':
-        if os.getenv("DATABASE_URL", None) is None:
-            raise Exception("DATABASE_URL environment variable not defined")
-        DATABASES = {
-            "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-        }
+elif len(sys.argv) ==1 or len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
