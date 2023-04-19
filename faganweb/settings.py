afc26d8c39f2fbaa2f7b3b05359da5813d2db1c3
faganjj <jfagan@MacBook-Air.local>
2023-04-19 15:27:59 -0400

    still trying to get DATABASES configuration correct

--- faganweb/settings.py
+++ faganweb/settings.py
@@ -139,7 +139,8 @@ if DEVELOPMENT_MODE is True:
 
 # The "len (sys.argv) == 1" condition below had to be added to allow Djang ORM-enabled
 # scripts to run on Digital Ocean
-elif len(sys.argv) <= 1 or len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     if os.getenv("DATABASE_URL", None) is None:
         raise Exception("DATABASE_URL environment variable not defined")
     DATABASES = {
