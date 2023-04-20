788d8483a7ce23ca7fb305e0da3d489ed9f10fdc
faganjj <jfagan@MacBook-Air.local>
2023-04-20 17:26:02 -0400

    still trying to get DATABASES configuration correct

--- faganweb/settings.py
+++ faganweb/settings.py
@@ -35,7 +35,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = [os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(","),
+    'stingray-app-gtqxh.ondigitalocean.app']
 
 
 # Application definition
