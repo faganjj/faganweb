9e72ec93545d1d2251eb069487e87f96b1114cdc
faganjj <jfagan@MacBook-Air.local>
2023-05-01 21:57:41 -0400

    fix bug in load_mlb_scores

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -37,7 +37,7 @@ def start():
 
 	scheduler.add_job(
 	  load_mlb_scores,
-	  trigger=CronTrigger(hour="3-4,21", minute=56),  
+	  trigger=CronTrigger(hour="3-4,22", minute=1),  
 	  id="load_mlb_scores",  # The `id` assigned to each job MUST be unique
 	  max_instances=1,
 	  replace_existing=True,
