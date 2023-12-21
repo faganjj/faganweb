dc2cd9b05fc45e8b7f905de487d6fc1ff2a8988c
faganjj <jfagan@MacBook-Air.local>
2023-12-21 13:14:51 -0500

    Fix bug with changing picks for games in progress

--- beat_the_odds/views.py
+++ beat_the_odds/views.py
@@ -58,7 +58,6 @@ def index(request):
 	# if the game can be legitimately picked.
 	compare_date = date.today()
 	compare_time = datetime.now().time()
-	# messages.info(request, "Hello")
 	# But if the contest record has been created for test purposes, set the 
 	# compare date to an arbitrary date in the past.
 	if contest.test_contest:
@@ -88,7 +87,7 @@ def index(request):
 			if compare_away in mypicks and compare_home in mypicks:
 				valid=False
 				messages.error(request, "You picked 2 winners for the same game. Please try again.")
-			# Also make sure the user has not made a pick for a game that has already started.
+			# Also make sure the user has not made a pick, or removed a pick, for a game that has already started.
 			picks = Pick.objects.filter(contest=contest, participant=user)
 			for pick in picks:
 				compare_pick = pick.abbrev + "," + pick.game_time.strftime("%H:%M")
@@ -96,12 +95,12 @@ def index(request):
 			if ((compare_away in mypicks and compare_away not in oldpicks) or (compare_home in mypicks and compare_home not in oldpicks)) \
 			and (compare_date > game.game_date or (compare_date == game.game_date and compare_time > game.game_time)):
 				valid=False
-				messages.error(request, "You made a pick for a game that already started. Please try again.")
+				messages.error(request, "You made a pick for a game that has already started. Please try again.")
 				return redirect('beat_the_odds:index')
 			if ((compare_away in oldpicks and compare_away not in mypicks) or (compare_home in oldpicks and compare_home not in mypicks)) \
 			and (compare_date > game.game_date or (compare_date == game.game_date and compare_time > game.game_time)):
 				valid=False
-				messages.error(request, "You changed a pick for a game that already started. Please try again.")
+				messages.error(request, "You changed a pick for a game that has already started. Please try again.")
 				return redirect('beat_the_odds:index')				
 		# If picks are valid, delete any prior picks and save the new picks.
 		# Also, create an initialized Result record for the user.  It serves as a
